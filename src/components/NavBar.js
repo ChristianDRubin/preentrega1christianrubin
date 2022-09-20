89383ef0b7a0256c000324847181ab2b220f7942
christiandrubin <c.d.rubin@gmail.com>
2022-09-20 19:32:44 -0300

    cambios menores

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,22 +9,18 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Autos</Nav.Link>
-            <Nav.Link href="#pricing">Motos</Nav.Link>
-            {/* <NavDropdown title="Categorias" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Ropa Hombre</NavDropdown.Item>              
-              <NavDropdown.Item href="#action/3.3">Ropa Mujer</NavDropdown.Item>             
-            </NavDropdown> */}
+            <Nav.Link href="#autos">Autos</Nav.Link>
+            <Nav.Link href="#motos">Motos</Nav.Link>            
           </Nav>
-          <Nav>
-            {/* <Nav.Link href="#deets">Perfil</Nav.Link>             */}
+          <Nav>            
             <NavDropdown title="Perfil" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Mi cuenta</NavDropdown.Item>              
-              <NavDropdown.Item href="#action/3.2">Ver Carrito<CartWidget /></NavDropdown.Item>             
+              <NavDropdown.Item href="#action/3.2">Ver Carrito</NavDropdown.Item>             
               <NavDropdown.Item href="#action/3.3">Cerrar Sesion</NavDropdown.Item>             
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>        
+        <CartWidget />
       </Container>
     </Navbar>
     );
